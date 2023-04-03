4ee9446521ab2c121c89a3a369ec1ef077de3334
Jeremy Denquin <jeremy@getlago.com>
2023-04-03 15:58:15 +0200

    misc(version): bump to v0.27.1-beta

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -32,7 +32,7 @@ await build({
     // package.json properties
     name: "lago-javascript-client",
     sideEffects: false,
-    version: "v0.27.0-beta",
+    version: "v0.27.1-beta",
     description: "Lago JavaScript API Client",
     repository: {
       "type": "git",
