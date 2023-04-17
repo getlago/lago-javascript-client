f141d5af21f0ad6bb6fde7e533f7cc4f128eebb5
Vincent Pochet <vincent.pochet@gmail.com>
2023-04-17 11:15:59 +0200

    misc(version): bump to v0.29.0-beta

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -32,7 +32,7 @@ await build({
     // package.json properties
     name: "lago-javascript-client",
     sideEffects: false,
-    version: "v0.27.0-beta",
+    version: "v0.29.0-beta",
     description: "Lago JavaScript API Client",
     repository: {
       "type": "git",
@@ -44,7 +44,7 @@ await build({
       "API",
       "Client",
     ],
-    contributors: ["Lovro Colic", "Jérémy Denquin", "Arjun Yelamanchili"],
+    contributors: ["Lovro Colic", "Jérémy Denquin", "Arjun Yelamanchili", "Vincent Pochet"],
     license: "MIT",
     bugs: {
       "url": "https://github.com/getlago/lago-javascript-client/issues",
