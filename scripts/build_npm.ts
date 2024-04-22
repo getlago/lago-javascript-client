a4f511ec3312f368e2dc2ec17dc64dea50daa527
Vincent Pochet <vincent@getlago.com>
2024-04-22 10:40:27 +0200

    misc: Version 1.2.0

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -32,7 +32,7 @@ await build({
     // package.json properties
     name: "lago-javascript-client",
     sideEffects: false,
-    version: "v1.1.0",
+    version: "v1.2.0",
     description: "Lago JavaScript API Client",
     repository: {
       "type": "git",
