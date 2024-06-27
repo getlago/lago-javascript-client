0c0b8d0f253b82a888336c0596be170798cc1bc8
Romain Sempé <romain@getlago.com>
2024-06-27 13:47:01 +0200

    misc: Bump version from 1.5.0 to 1.6.0

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -32,7 +32,7 @@ await build({
     // package.json properties
     name: "lago-javascript-client",
     sideEffects: false,
-    version: "v1.5.0",
+    version: "v1.6.0",
     description: "Lago JavaScript API Client",
     repository: {
       "type": "git",
