cfc50e6fbc0d82417e166ef3763f105bd64ef1cb
Jeremy Denquin <jdenquin@gmail.com>
2025-05-13 12:06:22 +0200

    misc(version): bump to v1.27.1

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -32,7 +32,7 @@ await build({
     // package.json properties
     name: "lago-javascript-client",
     sideEffects: false,
-    version: "v1.26.0",
+    version: "v1.27.1",
     description: "Lago JavaScript API Client",
     repository: {
       type: "git",
