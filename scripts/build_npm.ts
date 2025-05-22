03223a97632d175ee1de119da7ef8d14bdf08e04
Ancor Cruz <hello@ancorcruz.com>
2025-05-22 10:09:55 +0100

    misc(version): bump to v1.28.1 (#66)
    * misc(version): bump to v1.28.1
    
    * revert changes in deno.lock

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -32,7 +32,7 @@ await build({
     // package.json properties
     name: "lago-javascript-client",
     sideEffects: false,
-    version: "v1.27.1",
+    version: "v1.28.1",
     description: "Lago JavaScript API Client",
     repository: {
       type: "git",
