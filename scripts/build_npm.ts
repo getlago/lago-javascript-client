6bed0c59a67870b1097a63c41115760a8e64b030
Vincent Pochet <vincent@getlago.com>
2025-06-27 09:38:00 +0200

    misc(version): Bump version to 1.31.0

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -32,7 +32,7 @@ await build({
     // package.json properties
     name: "lago-javascript-client",
     sideEffects: false,
-    version: "v1.30.0",
+    version: "v1.31.0",
     description: "Lago JavaScript API Client",
     repository: {
       type: "git",
