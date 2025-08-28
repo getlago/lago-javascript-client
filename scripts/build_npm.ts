f0ff20d74645b1c1d375fff6916f7e0549150bd9
Anna Velentsevich <annvelents@gmail.com>
2025-08-28 19:22:49 +0200

    bump client version

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -32,7 +32,7 @@ await build({
     // package.json properties
     name: "lago-javascript-client",
     sideEffects: false,
-    version: "v1.33.0",
+    version: "v1.33.1",
     description: "Lago JavaScript API Client",
     repository: {
       type: "git",
