6bb166b9d8f2b5d0ee72589e6384b3b81ce2c808
Ancor Cruz <hello@ancorcruz.com>
2026-01-26 10:36:30 +0000

    misc(version): Bump vestion to 1.40.0

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -32,7 +32,7 @@ await build({
     // package.json properties
     name: "lago-javascript-client",
     sideEffects: false,
-    version: "v1.36.0",
+    version: "v1.40.0",
     description: "Lago JavaScript API Client",
     repository: {
       type: "git",
